813764e2c4bec137cf70602bce53b529e3215795
lihuimin <lihuimin@iplas.com.cn>
2018-05-10 14:59:34 +0800

    修改service namespace的bug

--- dapeng-code-generator/src/main/scala/com/github/dapeng/code/generator/JavaGenerator.scala
+++ dapeng-code-generator/src/main/scala/com/github/dapeng/code/generator/JavaGenerator.scala
@@ -320,7 +320,7 @@ class JavaGenerator extends CodeGenerator {
       import java.util.concurrent.Future;
       import java.util.ServiceLoader;
       import {service.namespace.substring(0, service.namespace.lastIndexOf(".")) + "." + service.name + "AsyncCodec.*"};
-      import {service.namespace + "." + service.name };
+      import {service.namespace+ "." + service.name }Async;
 
       /**
       {notice}
